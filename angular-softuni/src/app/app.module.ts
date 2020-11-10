c14ebb2eafb5093d77cb562ed87a1c8b0c6e1c11
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-11 00:13:41 +0200

    integrate bootstrap template

--- angular-softuni/src/app/app.module.ts
+++ angular-softuni/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = []
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
