ff44e3b2448b577523fec68255b8b5c0e10bc0d2
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-11 10:54:19 +0200

    create header component

--- angular-softuni/src/app/app.module.ts
+++ angular-softuni/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 const routes: Routes = []
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
