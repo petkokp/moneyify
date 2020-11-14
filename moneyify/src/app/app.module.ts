09a8f1ad349bd280e476a2b549e66a45525b666c
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-15 00:08:07 +0200

    add angular forms module to the budget calculator

--- moneyify/src/app/app.module.ts
+++ moneyify/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
@@ -27,7 +28,8 @@ import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/bud
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- moneyify/src/app/budget-add-item-form/budget-add-item-form.component.ts
+++ moneyify/src/app/budget-add-item-form/budget-add-item-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { BudgetItem } from 'src/shared/models/budget-item.module';
 
 @Component({
   selector: 'app-budget-add-item-form',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BudgetAddItemFormComponent implements OnInit {
 
+  @Input() item: BudgetItem = new BudgetItem('', null);
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+    
   }
 
 }
