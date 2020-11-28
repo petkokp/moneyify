ac29d3fd98096d74886c593d3e57c541af484130
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-28 12:37:31 +0200

    install auth packages and implement some methods

---
+++ api/db/models/user.model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
+
+const jwtSecret = "617776851226641fsdklafjasfakljfsklfjd7148523";
+
+const UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    },
+    sessions: [{
+        token: {
+            type: String,
+            required: true
+        },
+        expiresAt: {
+            type: Number,
+            required: true
+        }
+    }]
+});
+
+UserSchema.methods.toJSON = function () {
+    const user = this;
+    const userObject = user.toObject();
+
+    return _.omit(userObject, ['password', 'sessions']);
+}
+
+UserSchema.methods.generateAccessAuthToken = function () {
+    const user = this;
+    return new Promise((resolve, reject) => {
+        jwt.sign({ _id: user._id.toHexString() }, jwtSecret, { expiresIn: "15m" }, (err, token) => {
+            if (!err) {
+                resolve(token);
+            } else {
+                reject();
+            }
+        })
+    })
+}
