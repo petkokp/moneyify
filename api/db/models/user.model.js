079cc7fdc96fe966371d13bee130d74878fa1220
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-28 12:56:18 +0200

    implement helper methods

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -138,3 +138,29 @@ UserSchema.pre('save', function (next) {
     }
 });
 
+/* HELPER METHODS */
+
+let saveSessionToDatabase = (user, refreshToken) => {
+    return new Promise((resolve, reject) => {
+        let expiresAt = generateRefreshTokenExpiryTime();
+
+        user.sessions.push({ 'token': refreshToken, expiresAt });
+
+        user.save().then(() => {
+            return resolve(refreshToken);
+        }).catch((e) => {
+            reject(e);
+        });
+    })
+}
+
+let generateRefreshTokenExpiryTime = () => {
+    let daysUntilExpire = "10";
+    let secondsUntilExpire = ((daysUntilExpire * 24) * 60) * 60;
+    return ((Date.now() / 1000) + secondsUntilExpire);
+}
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = { User }
+
