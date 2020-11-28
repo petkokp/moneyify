d4f5e8f304a85cf4a8381bc0bcb1803d771387ca
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-28 13:15:53 +0200

    implement authentication middleware

--- api/app.js
+++ api/app.js
@@ -15,6 +15,62 @@ app.use(function(req, res, next) {
     next();
   });
 
+/* AUTHENTICATION MIDDLEWARE */
+
+let authenticate = (req, res, next) => {
+    let token = req.header('x-access-token');
+
+    jwt.verify(token, User.getJWTSecret(), (err, decoded) => {
+        if (err) {
+            res.status(401).send(err);
+        } else {
+            req.user_id = decoded._id;
+            next();
+        }
+    });
+}
+
+let verifySession = (req, res, next) => {
+    let refreshToken = req.header('x-refresh-token');
+
+    let _id = req.header('_id');
+
+    User.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            return Promise.reject({
+                'error': 'User not found. Make sure that the refresh token and user id are correct'
+            });
+        }
+
+        req.user_id = user._id;
+        req.userObject = user;
+        req.refreshToken = refreshToken;
+
+        let isSessionValid = false;
+
+        user.sessions.forEach((session) => {
+            if (session.token === refreshToken) {
+                if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            next();
+        } else {
+            return Promise.reject({
+                'error': 'Refresh token has expired or the session is invalid'
+            })
+        }
+
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+}
+
+/* ROUTES */
+
 app.get('/lists', (req, res) => {
     List.find({}).then((lists) => {
         res.send(lists);
