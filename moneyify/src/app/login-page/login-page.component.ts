2d6550990dd6d687b4a62fc49daa6443737f6ab7
petkopetkov987 <61232356+petkopetkov987@users.noreply.github.com>
2020-11-28 22:55:17 +0200

    update styles

--- moneyify/src/app/login-page/login-page.component.ts
+++ moneyify/src/app/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { HttpResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -9,14 +10,19 @@ import { AuthService } from '../auth.service';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
   onLoginButtonClicked(email: string, password: string) {
     this.authService.login(email, password).subscribe((res: HttpResponse<any>) => {
+      if (res.status === 200) {
+        this.router.navigate(['']);
+      }
+      
       console.log(res);
+
     });
   }
 
